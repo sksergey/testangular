9516eab9dbe0e2495cad79f1c105114aa668ea18
sksergey <skovorodkinsergey86@gmail.com>
2018-05-17 18:54:02 +0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test';
+  signinForm: FormGroup;
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit() {
+        this.initForm();
+    }
+
+    isControlInvalid(controlName: string): boolean {
+        const control = this.signinForm.controls[controlName];
+
+        const result = control.invalid && control.touched;
+
+        return result;
+    }
+
+    private initForm() {
+        this.signinForm = this.fb.group({
+            email: ['', [
+                Validators.required,
+                Validators.email
+            ]
+            ],
+            password: ['', [
+                Validators.required,
+                Validators.min(4)
+            ]
+            ]
+        });
+    }
 }
+
+
--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+//import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
-  styleUrls: ['./signin.component.css']
+  styleUrls: ['./signin.component.css'],
+  //imports: [ ReactiveFormsModule ]
 })
 export class SigninComponent implements OnInit {
 
